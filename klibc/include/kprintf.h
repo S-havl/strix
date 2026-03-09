4101d103bf4fb456426bd38fc608ba77b4a2d8ed
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-09 17:48:32 -0500

    Continuing with the development

--- klibc/include/kprintf.h
+++ klibc/include/kprintf.h
@@ -2,6 +2,7 @@
 #define KPRINTF_H
 
 #include <stdint.h>
+#include <stddef.h>
 
 void kprintf(const uint8_t* str);
 
--- klibc/src/kprintf.c
+++ klibc/src/kprintf.c
@@ -1,7 +1,9 @@
 #include <stdint.h>
+#include <stddef.h>
+
 #include "kprintf.h"
 
-static volatile uint16_t* const VGA_MEMORY = (const volatile uint16_t*)B8000;
+static volatile uint16_t* const VGA_MEMORY = (const volatile uint16_t*)0xB8000;
 
 void kprintf(const uint8_t* str) {
     
