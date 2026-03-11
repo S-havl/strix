285f7c97a4f0f141847946b8032e25e617a449c8
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-11 11:04:52 -0500

    Improve the kprintf function with line breaks and add a method to clear the screen.

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,7 +4,11 @@
 
 void _start(void) {
     clear_screen();
-    kprintf("Hello world from the kernel!");
+    kprintf("Hello world from the kernel!\n");
+    kprintf("Test1.\n");
+    kprintf("Test2.\n");
+    kprintf("Test3.\n");
+    kprintf("Everything perfect.\n");
     while (1) {
         __asm__("hlt");
     }
--- libk/src/kprintf.c
+++ libk/src/kprintf.c
@@ -4,12 +4,22 @@
 
 #define VGA_MEMORY 0xB8000
 #define VGA_COLOR 0x0F
+#define VGA_WIDTH 80
 
 static volatile uint16_t* const vga = (volatile uint16_t*)VGA_MEMORY;
+static size_t cursor = 0;
 
 void kprintf(const char* str) {
     for (size_t i = 0; str[i] != '\0'; i++) {
-        vga[i] = ((uint16_t)VGA_COLOR << 8) | (uint8_t)str[i];
+
+        if (str[i] == '\n') {
+            cursor += VGA_WIDTH - (cursor % VGA_WIDTH);
+            continue;
+        }
+
+        if (cursor >= 80 * 25) cursor = 0;
+        vga[cursor] = ((uint16_t)VGA_COLOR << 8) | (uint8_t)str[i];
+        cursor++;
     }
 }
 
