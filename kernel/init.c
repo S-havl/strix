99f38614f4945dfdde57608eae14322a7fac15bd
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-12 09:15:43 -0500

    Improving the initialization flow

---
+++ kernel/init.c
@@ -0,0 +1,37 @@
+#include <arch/x86_64/gdt.h>
+#include <arch/x86_64/idt.h>
+#include <arch/x86_64/pic.h>
+
+#include <drivers/timer/pit.h>
+#include <drivers/input/keyboard.h>
+
+#include <kprintf.h>
+
+void kernel_init() {
+    kprintf("Starting kernel...\n");
+
+    gdt_init();
+    kprintf("GDT initialized\n");
+
+    tss_init();
+    kprintf("TSS initialized\n");
+    
+    idt_init();
+    kprintf("IDT initialized\n");
+
+    pic_init();
+    kprintf("PIC initialized\n");
+    
+    pit_init();
+    kprintf("PIT initialized\n");
+
+    keyboard_init();
+    kprintf("KEYBOARD initialized\n");
+
+    asm volatile("sti");
+    kprintf("Interruptions enable.\n");
+    
+    while(1) {
+        asm volatile("hlt");
+    }
+}
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,15 +1,5 @@
-#include <stdint.h>
-#include <stddef.h>
-#include <kprintf.h>
+#include <kernel/init.h>
 
 void _start(void) {
-    clear_screen();
-    kprintf("Hello world from the kernel!\n");
-    kprintf("Test1.\n");
-    kprintf("Test2.\n");
-    kprintf("Test3.\n");
-    kprintf("Everything perfect.\n");
-    while (1) {
-        __asm__("hlt");
-    }
+    kernel_init();
 }
