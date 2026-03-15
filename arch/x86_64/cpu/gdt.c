c79656e739dd8ee5c6f131fbf0f40f2c47453c69
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-15 15:16:00 -0500

    Define GDT structures in kernel

--- arch/x86_64/cpu/gdt.c
+++ arch/x86_64/cpu/gdt.c
@@ -1,4 +1,19 @@
 #include <stdint.h>
 #include <gdt.h>
 
+#pragma pack(push, 1)
+struct GDTEntry {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
+};
+
+struct GDTR {
+    uint16_t limit;
+    uint64_t base;
+};
+#pragma pack(pop)
 
