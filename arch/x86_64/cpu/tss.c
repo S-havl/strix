7d39b4bc00f95304a2e13b172bfdf0639708dcd0
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-15 15:29:02 -0500

    Define TSS structure in kernel and gtd descriptor for TSS...

--- arch/x86_64/cpu/tss.c
+++ arch/x86_64/cpu/tss.c
@@ -1,2 +1,31 @@
 #include <stdint.h>
 #include <tss.h>
+
+struct GDTEntry64 {
+    uint16_t limit_low;
+    uint16_t base_low;
+    uint8_t  base_middle;
+    uint8_t  access;
+    uint8_t  granularity;
+    uint8_t  base_high;
+    uint32_t base_upper;
+    uint32_t reserved;
+} __attribute__((packed));
+
+struct TSS {
+    uint32_t reserved0;
+    uint64_t rsp0;
+    uint64_t rsp1;
+    uint64_t rsp2;
+    uint64_t reserved1;
+    uint64_t ist1;
+    uint64_t ist2;
+    uint64_t ist3;
+    uint64_t ist4;
+    uint64_t ist5;
+    uint64_t ist6;
+    uint64_t ist7;
+    uint64_t reserved2;
+    uint16_t reserved3;
+    uint16_t io_map_base;
+} __attribute__((packed));
--- arch/x86_64/cpu/tss.h
+++ arch/x86_64/cpu/tss.h
@@ -4,6 +4,7 @@
 #include <stdint.h>
 
 struct GDTEntry64;
+struct TSS;
 
 void set_gdt_tss(struct GDTEntry64 *entry, uint64_t base, uint32_t limit, uint8_t access, uint8_t flags);
 
