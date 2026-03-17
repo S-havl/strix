5a2836f8c780f923fa735a22a90233bd721b4d34
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-03-17 14:34:33 -0500

    Write the function to write the GDT structure

--- arch/x86_64/cpu/gdt.c
+++ arch/x86_64/cpu/gdt.c
@@ -22,5 +22,10 @@ struct GDTR {
 struct GDTEntry gdt[GDTEntry];
 
 void set_gdt_entry(struct GDTEntry *entry, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags) {
-    ;
+    entry->base_low    = base & 0xFFFF;
+    entry->base_middle = (base >> 16) & 0xFF;
+    entry->base_high   = (base >> 24) & 0xFF;
+    entry->limit_low   = limit & 0xFFFF;
+    entry->granularity = ((limit >> 16) & 0x0F) | (flags & 0xF0);
+    entry->access      = access;
 }
