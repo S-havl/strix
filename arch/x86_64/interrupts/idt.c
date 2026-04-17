d3d0c2601bf86567a22155c5cfa24625bac63f12
S-havl <pansillomcvalentincampostorres@gmail.com>
2026-04-16 19:31:15 -0500

    feat: add x86_64 IDT entry structure

--- arch/x86_64/interrupts/idt.c
+++ arch/x86_64/interrupts/idt.c
@@ -1,4 +1,16 @@
 #include <stdint.h>
 #include <arch/x86_64/interrupts/idt.h>
 
+#pragma pack(push, 1)
+struct IDTEntry {
+    uint16_t offset_low;
+    uint16_t selector;
+    uint8_t  ist;
+    uint8_t  type_attr;
+    uint16_t offset_mid;
+    uint32_t offset_high;
+    uint32_t zero;
+};
+
+
 
